6fdca16735bbf087af2f12d721e027e93e73fc20
Dmitriy <dima.blinkov@gmail.com>
2013-07-17 00:42:53 +0100

    db settings fix

--- wsgi/config.py
+++ wsgi/config.py
@@ -1,10 +1,10 @@
 import os
 
-#dbhost = os.environ['OPENSHIFT_MYSQL_DB_HOST'] #:$OPENSHIFT_MYSQL_DB_PORT'
+dbhost = os.environ['OPENSHIFT_MYSQL_DB_HOST'] #:$OPENSHIFT_MYSQL_DB_PORT'
 
 dbuser = 'adminuWJebAu'
 dbpass = 'CbbTZnik1KrN'
 dbname = 'enigma53'
 
-DB_URI = 'mysql://' + 'root' + ':' + 'root' + '@' + '127.0.0.1' + '/' +'qnet'
-#DB_URI = 'mysql://' + dbuser + ':' + dbpass + '@' + dbhost + '/' +dbname
+#DB_URI = 'mysql://' + 'root' + ':' + 'root' + '@' + '127.0.0.1' + '/' +'qnet'
+DB_URI = 'mysql://' + dbuser + ':' + dbpass + '@' + dbhost + '/' +dbname
