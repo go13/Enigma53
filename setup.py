6f1cb8caa159f3155169afa1feb832133440caf0
Dmitriy <dima.blinkov@gmail.com>
2013-07-18 21:48:53 +0100

    trying to remove jinja2

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='enigma53',
       author='Dmitro',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['flask', 'Jinja2', 'Flask-FlatPages', 'sqlalchemy', \
+      install_requires=['flask', 'Flask-FlatPages', 'sqlalchemy', \
                         'mysql-python', 'flask-sqlalchemy', 'flask-login', 'requests', 'Flask-WTF', 'scrubber'],
      )
