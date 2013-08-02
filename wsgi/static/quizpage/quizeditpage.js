8777044e8269c7c3419958ac813ba96df38ad3cf
Dmitriy <dima.blinkov@gmail.com>
2013-08-02 21:34:18 +0100

    - fixing tour issue when additional dialogs appeared

--- wsgi/static/quizpage/quizeditpage.js
+++ wsgi/static/quizpage/quizeditpage.js
@@ -50,22 +50,22 @@ steal(
                     onQuizesMenuItemClick(false);
                 }
             },{
-                element: ".wmd-button-row",
+                element: "#tabs-container .tab-pane.active .wmd-button-row",
                 title: "2/7",
                 content: "This is a Markdown editor which allows you to add <b>Checkboxes</b> and <b>Explanation sections</b>.",
                 placement: 'left'
             } ,{
-                element: ".wmd-button[title='Add Checkbox ?[+]']",
+                element: "#tabs-container .tab-pane.active .wmd-button[title='Add Checkbox ?[+]']",
                 title: "3/7",
                 content: "Checkboxes are marekd as <b>?[+]</b> for correct answers and <b>?[-]</b> for incorrect.<br><br> You can either click here to add one or put it manually in the editor.<br><br>",
                 placement: 'left'
             }, {
-                element: ".wmd-button[title='Add Explanation %[ Put text here ]%']",
+                element: "#tabs-container .tab-pane.active .wmd-button[title='Add Explanation %[ Put text here ]%']",
                 title: "4/7",
                 content: "Explanation sections are marked as <br><br><b>%[ This explanation text is shown on the page of results ]%</b><br><br> You can either click here to add one or put it manually to the editor</b>.",
                 placement: 'left'
             }, {
-                element: ".wmd-input",
+                element: "#tabs-container .tab-pane.active .wmd-input",
                 title: "5/7",
                 content: "Put the following text to see it rendered in the field below the editor: <br><br>" +
                 "Who is the main character of Lewis Carroll's book?<br><br>" +
@@ -75,7 +75,7 @@ steal(
                 "%[ The book is 'Alice in wonderland' ]%",
                 placement: 'left'
             },{
-                element: ".wmd-button[title='Save Question']",
+                element: "#tabs-container .tab-pane.active .wmd-button[title='Save Question']",
                 title: "6/7",
                 content: "Click this button to save current question.",
                 placement: 'left'
